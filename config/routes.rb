d92bba016be6452a0e6ea5de81f751b689ce7423
Gillian Wenhold <gwenhold@mac.com>
2019-07-25 13:08:35 -0400

    Added user sign up form & view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "static#home"
+  get "/login", to: "sessions#new", as: "login"
+  get "/signup", to: "users#new"
+
   resources :cohorts do
     resources :tasks, only: [:index, :show]
   end
