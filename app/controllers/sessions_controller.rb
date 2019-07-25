a30e2add7a7a194c72a4dd91e6274cad3c891478
Gillian Wenhold <gwenhold@mac.com>
2019-07-25 16:44:40 -0400

    Created login flow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    @user = User.find_by(name: params[:user][:name])
-    return head(:forbidden) unless @user.authenticate(params[:user][:password])
+    @user = User.find_by(username: params[:username])
+    return head(:forbidden) unless @user.authenticate(params[:password])
     session[:user_id] = @user.id
     redirect_to root_path
   end
