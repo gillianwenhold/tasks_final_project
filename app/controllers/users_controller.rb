0b4b348e942a9846a47ae5bf8df466aa0aa764d8
Gillian Wenhold <gwenhold@mac.com>
2019-07-25 12:50:02 -0400

    created users views, updated with has_secure_password, started with sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,41 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    return redirect_to root_path unless @user.save
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
+  end
+
+  def show; end
+
+  def update; end
+
+private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :username,
+      :password,
+      :first_name,
+      :last_name,
+      :grade,
+      :cohort_id
+    )
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   belongs_to :cohort
   has_many :user_tasks
   has_many :tasks, through: :user_tasks
+  has_secure_password
 end
