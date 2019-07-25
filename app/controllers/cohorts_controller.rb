36aedc15d5293471c7522b98642cb184ca1faa01
Gillian Wenhold <gwenhold@mac.com>
2019-07-25 10:36:49 -0400

    Created main controllers, views for cohorts, started building out cohort model

---
+++ app/controllers/cohorts_controller.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class CohortsController < ApplicationController
+  def index
+    @cohorts = Cohort.all
+  end
+
+  def new;
+    @cohort = Cohort.new
+  end
+
+  def create;
+    @cohort = Cohort.create(cohort_params)
+    if @cohort.save
+      redirect_to cohort_path(@cohort)
+    else
+      redirect_to new_cohort_path
+    end
+  end
+
+  def show; end
+
+private
+
+  def cohort_params
+    params.require(:cohort).permit(:name, :description)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cohorts do
+    resources :tasks, only: [:index, :show]
+  end
+  resources :tasks
+  resources :users, only: [:index, :show, :new, :create]
 end
