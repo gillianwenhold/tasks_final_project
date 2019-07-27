19a6a21b57a6a68fb8e06659b96113ee3edbb467
Gillian Wenhold <gwenhold@mac.com>
2019-07-27 15:36:31 -0400

    Added complete/incomplete scope, refactored user show page

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,6 +5,8 @@ class Task < ApplicationRecord
   has_many :user_tasks
   has_many :users, through: :user_tasks
   validates :due_date, presence: true
+  scope :completed, -> { where(complete: true) }
+  scope :incomplete, -> { where(complete: false) }
 
   def due
     due_date.strftime("%b %d, %Y")
