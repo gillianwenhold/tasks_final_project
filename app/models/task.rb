f1a9e7488249f4d9f478b9fb687cb14e61af031e
Gillian Wenhold <gwenhold@mac.com>
2019-07-26 22:21:04 -0400

    Updated CSS style for links, updated tasks index view to a table, moved task list for individuals & cohorts to a partial

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,6 +4,7 @@ class Task < ApplicationRecord
   belongs_to :cohort
   has_many :user_tasks
   has_many :users, through: :user_tasks
+  validates :due_date, presence: true
 
   def due
     due_date.strftime("%b %d, %Y")
