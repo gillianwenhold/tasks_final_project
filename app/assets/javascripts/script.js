0ddc9e27a18f096d3bf01bba73621c7ac89a44ce
Gillian Wenhold <gwenhold@mac.com>
2019-08-26 16:18:29 -0400

    Updated task show page to included claimed by in its own div, updated claimTask to match

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -4,14 +4,16 @@ $(document).ready(function() {
 
 function claimTask(event) {
   alert("Prevented Default");
-  var claimed = $(this).serialize();
+  var claimed = $(event).serialize();
+  debugger;
   var posting = $.post('/user_tasks', claimed);
-
+  posting.done(function() {
+    $("#taskUsers").append("Added User")
+  });
 }
 
 function attachListeners() {
   $("form#claim_task").submit(function(event) {
-    debugger;
     event.preventDefault();
     claimTask(this);
   })
