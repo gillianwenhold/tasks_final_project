1c2f90b404c3f99b2f2c1627b783edb9147f1072
Gillian Wenhold <gwenhold@mac.com>
2019-09-01 09:42:22 -0400

    Added ids to complete partial/task show page, created mark complete methods that changes html to complete

--- app/assets/javascripts/complete_task.js
+++ app/assets/javascripts/complete_task.js
@@ -1,9 +1,26 @@
-class Task() {
+$(document).ready(function() {
+  attachListeners();
+});
+
+class Task {
   constructor(description, priority, due_date, complete, group_id) {
-    this._description = description;
-    this._priority = priority;
-    this._due_date = due_date;
-    this._complete = complete;
-    this._group_id = group_id;
+    this.description = description;
+    this.priority = priority;
+    this.due_date = due_date;
+    this.complete = complete;
+    this.group_id = group_id;
   }
+  markComplete() {
+    this.complete = true;
+    $("#task_status").attr('class', 'done')
+    $("#task_status").text("COMPLETE")
+  }
+}
+
+function attachListeners() {
+  $("form#complete_task").submit(function(event) {
+    event.preventDefault();
+    var task = new Task;
+    task.markComplete();
+  })
 }
