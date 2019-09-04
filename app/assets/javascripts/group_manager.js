c85d3ca467c44a035ff26ce4b8aa93805cb7cdc2
Gillian Wenhold <gwenhold@mac.com>
2019-09-04 15:03:15 -0400

    Added class to user count on show page, updated js to oo with group objects

--- app/assets/javascripts/group_manager.js
+++ app/assets/javascripts/group_manager.js
@@ -2,14 +2,31 @@ $(document).ready(function() {
   attachListeners();
 });
 
+class Group {
+  constructor(data) {
+    this.id = data.id;
+    this.name = data.name;
+    this.description = data.description;
+    this.users = []
+  }
+
+  getUsers(data) {
+    for (let i=0; i<data.length; i++) {
+      this.users.push(data[i].username);
+    }
+  }
+}
+
 function nextGroup() {
-  alert("In next group")
-  var nextId = parseInt($(".js-next").attr("data-id")) + 1;
+  let nextId = parseInt($(".js-next").attr("data-id")) + 1;
   $.get("/groups/" + nextId + ".json", function(data) {
-    var group = data;
+    let group = new Group(data);
     console.log(group);
+    group.getUsers(data.users);
+    console.log(group.users)
     $(".group-name").text(group.name);
     $(".group-description").text(group.description);
+    $(".group-count").text("(" + group.users.length + " Users)");
     $(".js-next").attr("data-id", group.id);
     return false
   });
