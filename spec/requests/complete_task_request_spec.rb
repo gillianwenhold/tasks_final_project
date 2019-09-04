46e9c13a44c9df55f41aeb3fdb114160fea0b05f
Gillian Wenhold <gwenhold@mac.com>
2019-09-04 16:03:40 -0400

    Created request tests for completing a task, and loading single resources

---
+++ spec/requests/complete_task_request_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Complete Task Requests", type: :request do
+
+end
---
+++ spec/requests/single_resource_request_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Single Resource Request", type: :request do
+
+end
