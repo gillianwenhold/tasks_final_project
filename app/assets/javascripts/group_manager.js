bd6bfffdb396e4cc015b402e892e81c1ed416c96
Gillian Wenhold <gwenhold@mac.com>
2019-09-04 14:50:03 -0400

    removed turbolinks, fixed nextGroup function

--- app/assets/javascripts/group_manager.js
+++ app/assets/javascripts/group_manager.js
@@ -3,20 +3,21 @@ $(document).ready(function() {
 });
 
 function nextGroup() {
-  alert("in nex tfunction");
+  alert("In next group")
   var nextId = parseInt($(".js-next").attr("data-id")) + 1;
   $.get("/groups/" + nextId + ".json", function(data) {
     var group = data;
     console.log(group);
-    $(".group-name").text(group["title"]);
-    $(".group-description").text(group["description"]);
-    $(".js-next").attr("data-id", group["id"]);
+    $(".group-name").text(group.name);
+    $(".group-description").text(group.description);
+    $(".js-next").attr("data-id", group.id);
+    return false
   });
 }
 
-
 function attachListeners() {
-  $(".js-next").on("click", function() {
+  $(".js-next").on("click", function(event) {
+    event.preventDefault();
     nextGroup();
   });
 }
