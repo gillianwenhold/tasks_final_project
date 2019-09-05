6e7319361318999f9e714d88333eb2820a8d83f1
Gillian Wenhold <gwenhold@mac.com>
2019-09-05 14:04:43 -0400

    Fixed rubocop offenses serializers and targetrubyversion

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class GroupSerializer < ActiveModel::Serializer
-  attributes :name, :description,
+  attributes :name, :description
   has_many :users
 end
--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class TaskSerializer < ActiveModel::Serializer
-  attributes :priority, :due_date, :description, :complete,
+  attributes :priority, :due_date, :description, :complete
   belongs_to :group
   has_many :user_tasks
   has_many :users, through: :user_tasks
