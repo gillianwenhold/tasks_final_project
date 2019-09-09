2e93dcb320eba40d34df7cd830773e4a0ff0c0e4
Gillian Wenhold <gwenhold@mac.com>
2019-09-09 12:24:31 -0400

    Created rspec tests for making a new group

--- spec/requests/group_requests_spec.rb
+++ spec/requests/group_requests_spec.rb
@@ -4,40 +4,20 @@ require "rails_helper"
 require "pry"
 
 RSpec.describe "Group requests", :type => :request do
-  before do
+  before :each do
     Task.destroy_all
     Group.destroy_all
     User.destroy_all
-    group = create(:group)
-    user = create(:user, group: group)
+    @group = create(:group)
+    @user = create(:user, group: @group, admin: true)
+    allow_any_instance_of(ApplicationController).to receive(:require_login).and_return(@user.id)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    allow_any_instance_of(Group).to receive_messages(name: "New Group", description: "group description")
   end
 
-=begin
   it "Creates a group" do
-    allow_any_instance_of(Group).to receive(:method).and_return(value)
     headers = { "ACCEPT" => "application/json" }
     post "/groups", params: { :group => { name: "New Group", description: "group description" } }, headers: headers
-    expect(response).to have_http_status(:created)
+    expect(Group.all[1].description).to eq("group description")
   end
-
-  it "Creates a new task and redirects to the show page" do
-    group = create(:group)
-    date = Time.now
-    due_date = date.strftime("%Y-%m-%d")
-    headers = { "ACCEPT" => "application/json", }
-    post "/tasks", :params => { :task => { priority: 1, due_date: due_date, description: "new task", group_id: group.id } }, headers: headers
-    expect(response).to redirect_to(assigns(:task))
-  end
-
-  it "creates a new task" do
-    headers = {
-      "ACCEPT" => "application/json",
-      "Content-Type" => "application/json",
-    }
-    post "/tasks", :params => { :task => {:description => "New task"} }, :headers => headers
-
-    expect(response.content_type).to eq("application/json")
-    expect(response).to have_http_status(:created)
-  end
-=end
 end
