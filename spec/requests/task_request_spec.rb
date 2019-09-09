b5daa73c1ab884509e4b3ba2891451e3850ef9bd
Gillian Wenhold <gwenhold@mac.com>
2019-09-09 12:19:39 -0400

    Added more tests to task_request_spec

--- spec/requests/task_request_spec.rb
+++ spec/requests/task_request_spec.rb
@@ -12,11 +12,18 @@ RSpec.describe "Task requests", :type => :request do
     @user = create(:user, group: @group)
     @task = create(:task)
     allow_any_instance_of(ApplicationController).to receive(:require_login).and_return(@user.id)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     allow_any_instance_of(Task).to receive_messages(priority: 1, due_date: Time.now, description: "New Task", group: @group)
   end
 
+  it "GETs the index page successfully" do
+    get "/tasks"
+    expect(response.status).to eq(200)
+  end
+
   it "Creates a new task" do
-    post "/tasks", params: { :task => { priority: 1, due_date: Time.now, description: "New Task" } }, headers: { "ACCEPT" => "application/json" }
+    headers = { "ACCEPT" => "application/json" }
+    post "/tasks", params: { :task => { priority: 1, due_date: Time.now, description: "New Task" } }, headers: headers
     expect(response).to redirect_to(assigns(:task))
   end
 
@@ -27,26 +34,4 @@ RSpec.describe "Task requests", :type => :request do
     patch post_url, params: { :task => { complete: true } }, headers: { "ACCEPT" => "application/json" }
     expect(@task.complete).to eq(true)
   end
-
-=begin
-  it "Creates a new task and redirects to the show page" do
-    group = create(:group)
-    date = Time.now
-    due_date = date.strftime("%Y-%m-%d")
-    headers = { "ACCEPT" => "application/json", }
-    post "/tasks", :params => { :task => { priority: 1, due_date: due_date, description: "new task", group_id: group.id } }, headers: headers
-    expect(response).to redirect_to(assigns(:task))
-  end
-
-  it "creates a new task" do
-    headers = {
-      "ACCEPT" => "application/json",
-      "Content-Type" => "application/json",
-    }
-    post "/tasks", :params => { :task => {:description => "New task"} }, :headers => headers
-
-    expect(response.content_type).to eq("application/json")
-    expect(response).to have_http_status(:created)
-  end
-=end
 end
